8160d94fbe48d9c791d3884c97690e3f127487f4
Zura Doghanadze <zurabi.doghanadze.1@iliauni.edu.ge>
2023-10-12 16:59:03 +0400

    add styled components

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -16,31 +16,123 @@ const Wrapper = styled.div`
     background-image: url(${MobileImage});
   }
 `;
+const TextContainer = styled.div`
+  max-width: 525px;
+  display: flex;
+  flex-direction: column;
+  align-items: self-start;
+  color: #ffffff;
+`;
+const Title = styled.h1`
+  font-size: 50px;
+  font-weight: 700;
+  line-height: 55px; /* 110% */
+  letter-spacing: -0.521px;
+  text-align: start;
+  margin-bottom: 30px;
+`;
+const Text = styled.p`
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 26px;
+  padding-right: 40px;
+  text-align: start;
+`;
+const InputWrapper = styled.div`
+  width: 540px;
+  display: flex;
+  flex-direction: column;
+`;
+const InputTitle = styled.h2`
+  max-width: 540px;
+  padding: 17px 0;
+  border-radius: 10px;
+  background: #5e54a4;
+  box-shadow: 0px 8px 0px 0px rgba(0, 0, 0, 0.15);
+  letter-spacing: 0.368px;
 
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+`;
+const LeftTitle = styled.span`
+  font-weight: 00;
+  font-size: 15px;
+  margin-right: 5px;
+`;
+const RightTitle = styled.span`
+  font-size: 15px;
+  font-weight: 400;
+`;
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 20px;
+  margin-top: 24px;
+  padding: 40px 40px;
+  border-radius: 10px;
+  background: #fff;
+  box-shadow: 0px 8px 0px 0px rgba(0, 0, 0, 0.15);
+`;
+
+const Button = styled.button`
+  border: none;
+  border-radius: 5px;
+  background: #38cc8b;
+  box-shadow: 0px -4px 0px 0px rgba(0, 0, 0, 0.09) inset;
+  cursor: pointer;
+  color: #fff;
+  font-size: 15px;
+  font-weight: 600;
+  letter-spacing: 1px;
+  padding: 15px 0;
+
+  &:hover {
+    background: #77e2b3;
+  }
+`;
+const DescriptionContainer = styled.div`
+  font-size: 11px;
+  line-height: 26px;
+`;
+const LeftDescription = styled.span`
+  color: #bab7d4;
+  margin-right: 5px;
+`;
+const RightDescription = styled.span`
+  color: #ff7979;
+`;
 function Card() {
   return (
     <Wrapper>
-      <div>
-        <h1>Learn to code by watching others</h1>
-        <p>
+      <TextContainer>
+        <Title>Learn to code by watching others</Title>
+        <Text>
           See how experienced developers solve problems in real-time. Watching
           scripted tutorials is great, but understanding how developers think is
           invaluable.
-        </p>
-      </div>
-      <div>
-        <h2>Try it free 7 days then $20/mo. thereafter</h2>
-        <div>
+        </Text>
+      </TextContainer>
+      <InputWrapper>
+        <InputTitle>
+          <LeftTitle>Try it free 7 days</LeftTitle>
+          <RightTitle>then $20/mo. thereafter</RightTitle>
+        </InputTitle>
+        <InputContainer>
           <input type="text" />
           <input type="text" />
           <input type="text" />
           <input type="text" />
-        </div>
-        <button>CLAIM YOUR FREE TRIAL</button>
-        <span>
-          By clicking the button, you are agreeing to our Terms and Services
-        </span>
-      </div>
+          <Button>CLAIM YOUR FREE TRIAL</Button>
+          <DescriptionContainer>
+            <LeftDescription>
+              By clicking the button, you are agreeing to our
+            </LeftDescription>
+            <RightDescription>Terms and Services</RightDescription>
+          </DescriptionContainer>
+        </InputContainer>
+      </InputWrapper>
     </Wrapper>
   );
 }
